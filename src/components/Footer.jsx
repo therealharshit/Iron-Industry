048580cd040780c262c8792cea3b07e0eb439295
therealharshit <therealharshit014@gmail.com>
2025-01-01 20:44:40 +0530

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,13 +20,13 @@ export default function Footer() {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
-                                    <Link to="/" className="hover:underline">
-                                        Home
+                                    <Link to="/about" className="hover:underline">
+                                        About
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link to="/about" className="hover:underline">
-                                        About
+                                    <Link to="/contact" className="hover:underline">
+                                        Contact
                                     </Link>
                                 </li>
                             </ul>
